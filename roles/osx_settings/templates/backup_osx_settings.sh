8aa5e8da59fa914b0ced29e6322af4cf2ec701f9
Chris Chambers <magma.chambers@gmail.com>
2024-07-10 09:42:58 +0100

    Better names for backup script

--- roles/osx_settings/templates/backup_osx_settings.sh
+++ roles/osx_settings/templates/backup_osx_settings.sh
@@ -6,14 +6,14 @@ backup_location="{{ ansible_env.HOME }}/backups/osx_settings/"
 filename="$(date '+%Y-%m-%d').defaults.1"
 target="${backup_location}/${filename}"
 
-bump_osx_defaults_version_number() {
+bump_osx_settings_version_number() {
   echo "$(echo "$1" | awk -F. -v OFS=. '{$NF += 1 ; print}')"
 }
 
 main() {
     mkdir -p "$backup_location"
     while [[ -f $target ]]; do
-      target=$(bump_osx_defaults_version_number "$target")
+      target=$(bump_osx_settings_version_number "$target")
     done
     defaults read > "$target"
 }
